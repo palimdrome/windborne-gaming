6446a62a5b127c5b77472bad81b300fdc5c13a07
Jon Robien Jinon <robien.jinon@gmail.com>
2024-03-03 13:18:19 +0800

    Added Welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 // Components
 import Navbar from './components/Navbar'
+import Welcome from './components/Welcome'
 
 function App() {
 
   return (
     <div className='w-screen min-h-screen text-white'>
       <Navbar/>
+      <Welcome/>
     </div>
   )
 }
